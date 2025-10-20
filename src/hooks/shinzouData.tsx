70e07b2b175bff64941baa445ebb8e07e9d7a830
erillope <ericklorenzolopez1@gmail.com>
2025-10-20 11:34:39 -0500

    fix path

--- src/hooks/shinzouData.tsx
+++ src/hooks/shinzouData.tsx
@@ -71,7 +71,8 @@ export const shinshouChaptersVideos = [
     "https://www.youtube.com/watch?v=Y_j7gHOgYY4"
 ]
 
-export const shinshouChaptersLinks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(num => `/#/shinzou/shinshouRoku/chapter-${num}/`);
+export const shinshouChaptersLinks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].
+map(num => window.location.origin + window.location.pathname + `#/shinzou/shinshouRoku/chapter-${num}/`);
 
 export const tsuisouChaptersVideos = [
     "https://www.youtube.com/watch?v=HKsSGYCRGFk",
